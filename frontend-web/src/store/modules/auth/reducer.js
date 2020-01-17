491c4cf3c147699f23eb233f96ff326b96dad399
Rafael de Carvalho <rafael_c_105@hotmail.com>
2020-01-17 15:50:41 -0300

    feat: frontend-Web finished

--- frontend-web/src/store/modules/auth/reducer.js
+++ frontend-web/src/store/modules/auth/reducer.js
@@ -8,10 +8,24 @@ const INICIAL_STATE = {
 
 export default function auth(state = INICIAL_STATE, action) {
   switch (action.type) {
+    case '@auth/SIGN_IN_REQUEST':
+      return produce(state, draft => {
+        draft.loading = true;
+      });
     case '@auth/SIGN_IN_SUCCESS':
       return produce(state, draft => {
         draft.token = action.payload.token;
         draft.signed = true;
+        draft.loading = false;
+      });
+    case '@auth/SIGN_FAILURE':
+      return produce(state, draft => {
+        draft.loading = false;
+      });
+    case '@auth/SIGN_OUT':
+      return produce(state, draft => {
+        draft.token = null;
+        draft.signed = false;
       });
     default:
       return state;

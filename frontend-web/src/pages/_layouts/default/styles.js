0fd55708a75fe71a2a89928ad43186335a31f234
Rafael de Carvalho <rafael_c_105@hotmail.com>
2020-01-18 08:58:28 -0300

    fix: background height

--- frontend-web/src/pages/_layouts/default/styles.js
+++ frontend-web/src/pages/_layouts/default/styles.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  height: auto;
-  overflow: hidden;
+  height: 100%;
   background: linear-gradient(-90deg, #7159c1, #ab59c1);
 `;
